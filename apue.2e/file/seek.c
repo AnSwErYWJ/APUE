1aa7cea7093c3e9bcd54a7ce743298befc1a903a
AnSwErYWJ <ywj1993@yeah.net>
2015-07-15 13:49:33 +0800

    20150715

--- apue.2e/file/seek.c
+++ apue.2e/file/seek.c
@@ -1,11 +1,10 @@
-#include "apue.h"
-
-int
-main(void)
+#include<unistd.h>
+#include<stdio.h>
+int main()
 {
 	if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
-		printf("cannot seek\n");
+		printf("Cannot seek\n");
 	else
-		printf("seek OK\n");
-	exit(0);
+		printf("Seek OK\n");
+	return 0;
 }
