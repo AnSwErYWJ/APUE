5573be127e2a5aafa2eecacecd0240e338941f7e
AnSwErYWJ <ywj1993@yeah.net>
2015-07-29 21:02:09 +0800

    信号函数signal

---
+++ signal/signal.c
@@ -0,0 +1,32 @@
+/*************************************************************************
+	> File Name: signal.c
+	> Author: AnSwEr
+	> Mail: 1045837697@qq.com
+	> Created Time: 2015年07月29日 星期三 20时58分05秒
+ ************************************************************************/
+/*
+ *signal只处理一次信号
+ */
+
+
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+
+void ouch(int sig)
+{
+    printf("OUCH!-I GOT SIGNAL %d\n",sig);
+    (void)signal(SIGINT,SIG_DFL);
+}
+
+int main(void)
+{
+    (void)signal(SIGINT,ouch);
+
+    while(1)
+    {
+        printf("Hello\n");
+        sleep(1);
+    }
+    return 0;
+}
