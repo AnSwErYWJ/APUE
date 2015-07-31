712be6c495f1440fb3e429f5712c1427a394d26c
AnSwErYWJ <ywj1993@yeah.net>
2015-07-31 17:11:16 +0800

    临时文件

---
+++ stdio/tmpfile.c
@@ -0,0 +1,32 @@
+/*
+ *创建临时文件
+ */
+
+#include<stdlib.h>
+#include <stdio.h>
+#define MAXLINE 1024
+
+int main(void)
+{
+	char name[L_tmpnam], line[MAXLINE];
+	FILE *fp;
+
+	printf("%s\n", tmpnam(NULL));		/* first temp name */
+
+	tmpnam(name);						/* second temp name */
+	printf("%s\n", name);
+
+	if ((fp = tmpfile()) == NULL)		/* create temp file */
+		perror("tmpfile error");
+
+	fputs("one line of output\n", fp);	/* write to temp file */
+	
+    rewind(fp);							/* then read it back */
+	
+    if (fgets(line, sizeof(line), fp) == NULL)
+		perror("fgets error");
+	fputs(line, stdout);	/* print the line we wrote */
+
+    putchar('\0');
+	exit(0);
+}
